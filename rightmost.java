fc467d1c5302dcc52c736cbb30f55d2cedac1824
Noor <noorkoul890@gmail.com>
2026-05-04 14:01:39 +0530

    Add files via upload

---
+++ rightmost.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class rightmost {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter three non- negative integers: ");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        // last digit
+        int d1 = a % 10;
+        int d2 = b % 10;
+        int d3 = c % 10;
+
+        boolean result = (d1 == d2) || (d2 == d3) || (d1 == d3);
+
+        System.out.println("Result: " + result);
+    }
+}
